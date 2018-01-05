74d49d05c2593b8906dbe1ebe3cd5238e650534c
sonjer <sonjermorgado@gmail.com>
2018-01-05 17:11:47 -0600

    cambiosviernes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,18 @@ import {SpotifyService} from './services/spotify.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private gridApi;
-  columnDefs;
-  rowData;
-  data2: any []= [];
+  private defaultColDef;
+  private rowSelection;
+  private columnDefs;
+
+  data2: any [] = [];
 
   constructor(public _spotify: SpotifyService) {
-    this._spotify.getArtistas().then( valido => {
+    this._spotify.getArtistas().then(valido => {
       if (valido) {
         this.data2 = this._spotify.data;
         console.log(this.data2);
-      }else {
+      } else {
         console.log('No hay Artistas');
       }
     }).catch(error => {
@@ -26,17 +27,33 @@ export class AppComponent {
     });
 
     this.columnDefs = [
-      {headerName: 'ID', field: 'id', width: 300},
-      {headerName: 'NAME', field: 'name', width: 300},
-      {headerName: 'PHONE', field: 'phone', width: 300},
-      {headerName: 'USERNAME', field: 'username', width: 300},
-      {headerName: 'WEBSITE', field: 'website', width: 300},
-      {headerName: 'EMAIL', field: 'email', width: 300},
-      {headerName: 'COMPANY', field: 'company.name', width: 300},
-      {headerName: 'COMPANY BS' , field: 'company.bs', width: 300},
-      {headerName: 'COMPANY CATCHPHRASE', field: 'company.catchPhrase', width: 300}
+      {headerName: 'ID', field: 'id', width: 100},
+      {headerName: 'NAME', field: 'name', width: 220},
+      {headerName: 'PHONE', field: 'phone', width: 220},
+      {headerName: 'USERNAME', field: 'username', width: 150},
+      {headerName: 'WEBSITE', field: 'website', width: 150},
+      {headerName: 'EMAIL', field: 'email', width: 250},
+      {
+        headerName: 'ADDRESS', children: [
+          {headerName: 'CITY', columnGroupShow: 'closed', field: 'address.city', width: 300},
+          {headerName: 'CITY', columnGroupShow: 'open', field: 'address.city', width: 300},
+          {headerName: 'LAT', columnGroupShow: 'open', field: 'address.geo.lat', width: 300},
+          {headerName: 'LNG', columnGroupShow: 'open', field: 'address.geo.lng', width: 300},
+          {headerName: 'STREEET', columnGroupShow: 'open', field: 'address.suite', width: 300},
+          {headerName: 'ZIPCODE', columnGroupShow: 'open', field: 'address.zipcode', width: 300},
+        ]
+      },
+      {
+        headerName: 'COMPANY', children: [
+          {headerName: 'COMPANY NAME', columnGroupShow: 'closed', field: 'company.name', width: 300},
+          {headerName: 'COMPANY NAME', columnGroupShow: 'open', field: 'company.name', width: 300},
+          {headerName: 'BS', columnGroupShow: 'open', field: 'company.bs', width: 300},
+          {headerName: 'CATCHPHRASE', columnGroupShow: 'open', field: 'company.catchPhrase', width: 300},
+        ]
+      }
     ];
-
+    this.defaultColDef = {editable: true};
+    this.rowSelection = "multiple";
 
   }
 
